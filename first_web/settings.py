b8581800471f0cf8353e041e40af8575d7d343ac
Mgalazyn <mgalazynn@gmail.com>
2022-11-08 21:56:53 +0100

    making css of ws, adding images

--- first_web/settings.py
+++ first_web/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -49,7 +49,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware"
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 INTERNAL_IPS = [
@@ -126,6 +127,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = BASE_DIR, 'images'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Project
+from .forms import ProjectForm
 # Create your views here.
 # request handler
 
 
-def first_words(request):
-    context = {'name': 'Marcin\'s web'}
-    return render(request, 'hello.html', context)
+def main(request):
+    context = {'name': 'Projects'}
+    return render(request, 'main.html', context)
 
 
 def project(request):
@@ -14,3 +15,38 @@ def project(request):
     contex = {'projects': projects}
     return render(request, 'single_project.html', contex)
 
+
+def create_project(request):
+    form = ProjectForm()
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('main')
+    contex = {'form': form}
+    return render(request, 'project_form.html', contex)
+
+
+def update_project(request, pk):
+    projectt = Project.objects.get(id=pk)
+    form = ProjectForm(instance=projectt)
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, request.FILES, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('main')
+    contex = {'form': form}
+    return render(request, 'project_form.html', contex)
+
+
+def delete_project(request, pk):
+    project = Project.objects.get(id=pk)
+
+    if request.method =='POST':
+        project.delete()
+        return redirect('main')
+
+    contex = {'object': project}
+    return render(request, 'delete_object.html', contex)
