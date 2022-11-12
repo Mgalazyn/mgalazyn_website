f103ccb06a2b7bfa5f5b81fef4126959df6e2a39
Mgalazyn <m.galazynn@gmail.com>
2022-11-12 19:22:06 +0100

    adding css and changing it, theme installation

--- playground/forms.py
+++ playground/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Project
 
 
@@ -6,3 +7,16 @@ class ProjectForm(ModelForm):
     class Meta:
         model = Project
         fields = ['title', 'description', 'source_link', 'tags', 'featured_image']
+        widgets = {
+            'tags':forms.CheckboxSelectMultiple(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+
+        self.fields['title'].widget.attrs.update({'class':'input', 'placeholder': 'Add title'})
+
+        self.fields['description'].widget.attrs.update({'class':'input', 'placeholder': 'Add text'})
+
+        self.fields['source_link'].widget.attrs.update({'class':'input', 'placeholder': 'Add link'})
+        
